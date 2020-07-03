710605938fb7db2016cddd7c4ed0bf7801390a88
Samuel Bodin <samuel.bodin@algolia.com>
2020-07-03 11:55:12 +0200

    build

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-export function onPreBuild({ inputs }) {
+// Need to be sync with manifest.yml
+type Inputs = {
+  appId: string;
+  apiKey: string;
+}
+
+export function onPreBuild({ inputs }: { inputs: Inputs }) {
   console.log('Hello world from onPreBuild event!');
   console.log('Got inputs', inputs);
 }
