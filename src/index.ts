8de476a21c0653f0c70aa1b2092682b60add55f5
Matthieu Dumont <jerska@live.fr>
2020-09-18 21:43:08 +0200

    feat: add siteName & deployPrimeUrl

--- src/index.ts
+++ src/index.ts
@@ -28,10 +28,20 @@ export async function onSuccess(params: BuildParams) {
   const algoliaBaseUrl =
     process.env.ALGOLIA_BASE_URL || 'https://crawler.algolia.com';
   const algoliaApiKey = process.env.ALGOLIA_API_KEY;
+  const siteName =
+    process.env.NODE_ENV === 'development'
+      ? 'crawler-netlify-plugin'
+      : process.env.SITE_NAME;
+  const deployPrimeUrl =
+    process.env.NODE_ENV === 'development'
+      ? 'https://master--crawler-netlify-plugin.netlify.app'
+      : process.env.DEPLOY_PRIME_URL;
 
   if (!siteId) throw new Error('Missing SITE_ID');
   if (!branch) throw new Error('Missing BRANCH');
   if (!algoliaApiKey) throwExceptInDev('Missing ALGOLIA_API_KEY');
+  if (!siteName) throw new Error('Missing or invalid SITE_NAME');
+  if (!deployPrimeUrl) throw new Error('Missing DEPLOY_PRIME_URL');
 
   const endpoint = `${algoliaBaseUrl}/api/1/netlify/crawl`;
   const creds = `${siteId}:${algoliaApiKey || 'unused'}`;
@@ -41,7 +51,7 @@ export async function onSuccess(params: BuildParams) {
       Authorization: `Basic ${Buffer.from(creds).toString('base64')}`,
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ branch }),
+    body: JSON.stringify({ branch, siteName, deployPrimeUrl }),
   });
 
   console.log({
