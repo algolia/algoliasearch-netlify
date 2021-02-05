8b38eda29c4b895d00d3ab39581de2caea56d172
Sylvain Bellone <sylvain.bellone@algolia.com>
2021-02-05 18:36:33 +0100

    Move autocomplete-theme-classic into a dedicated file, for easier maintenance

--- frontend/src/templates.ts
+++ frontend/src/templates.ts
@@ -34,10 +34,10 @@ export const templates = {
         { href: record.url },
         createElement(
           'div',
-          { class: 'aa-Item aa-ItemContent' }, // aa-ItemContent is kept for compatibility with former CSS
+          { class: 'aa-ItemContent' },
           createElement(
             'div',
-            { class: 'aa-ItemIcon aa-ItemIcon--no-border aa-ItemSourceIcon' }, // aa-ItemSourceIcon is kept for compatibility with former CSS
+            { class: 'aa-ItemIcon' },
             createElement(
               'svg',
               { width: '20', height: '20', viewBox: '0 0 20 20' },
